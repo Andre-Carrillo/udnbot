7ebbee01420f29990e34c29f86a2cdc20c4535ea
Andre Carrillo <carrillomonteroandre@gmail.com>
2022-04-12 11:10:14 -0500

    made pastas be in pastas.json

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const {Client, Intents} = require('discord.js');
 const { token } = require('./config.json');
+const { pastas } = require('./pastas.json');
 
 //haciendo el cliente
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
@@ -11,8 +12,12 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', (msg)=>{
-	if(msg.channel.id=="963263187468308530" && msg.content.includes('flaca')){
-		msg.reply('te digo en 2 sencillos pasos en como hacer que te quiera, paso 1 cuando vayan a verse le miras a los ojos y le dices ya esta aqui tu macho que te sacara de este cerro, paso 2 le das 50 soles a mas y listo problema solucionado')
+	if(msg.channel.id=="963263187468308530"){
+		for(const pasta in pastas){
+			if (msg.content === pastas[pasta].text){
+				msg.reply(pastas[pasta].copypasta);
+			}
+		}
 	}
 });
 
