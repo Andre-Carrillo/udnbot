c8af89536abc8dd9e8d491783e996782adf0b5e6
Andre Carrillo <carrillomonteroandre@gmail.com>
2022-04-11 22:13:47 -0500

    made bot

--- bot.js
+++ bot.js
@@ -1,11 +1,20 @@
-const { Client, Intents } = require('discord.js');
+const {Client, Intents} = require('discord.js');
 const { token } = require('./config.json');
 
 //haciendo el cliente
-const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
-client.once('ready', () => {
+
+
+client.on('ready', () => {
 	console.log('Bot ready');
 });
 
+client.on('messageCreate', (msg)=>{
+	if(msg.channel.id=="963263187468308530" && msg.content==='hola bot'){
+		msg.reply('hola humano')
+	}
+});
+
 client.login(token);
+
