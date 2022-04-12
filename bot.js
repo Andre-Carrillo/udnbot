470b86ea45cff71584b1a8d9c86d0e57adf6507b
Andre Carrillo <carrillomonteroandre@gmail.com>
2022-04-12 11:13:24 -0500

    added general channel

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', (msg)=>{
-	if(msg.channel.id=="963263187468308530"){
+	if(msg.channel.id=="963263187468308530" || msg.channel.id=="955847068331679765" ){
 		for(const pasta in pastas){
 			if (msg.content === pastas[pasta].text){
 				msg.reply(pastas[pasta].copypasta);
