f7d547fcf1298346c8fd23a4c9a58f89601b8774
Andre Carrillo <carrillomonteroandre@gmail.com>
2022-04-14 15:39:31 -0500

    added beta of matrix functionalities

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const {Client, Intents} = require('discord.js');
 //const { token } = require('./config.json');
-const { pastas } = require('./pastas.json');
+//const { pastas } = require('./pastas.json');
 require('dotenv').config();
 const tokenenv = process.env.TOKEN;
+const {Matrix3} = require('./gauss.js');
 
 //haciendo el cliente
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
@@ -16,13 +17,41 @@ client.on('ready', () => {
 client.on('messageCreate', (msg)=>{
 	if(msg.author.bot){return}
 	if(msg.channel.id=="963263187468308530" || msg.channel.id=="955847068331679765" ){
+		const {pastas} = require('./pastas.json')
+		find: {
 		for(const pasta in pastas){
 			if (msg.content.includes(pastas[pasta].text)){
 				msg.reply(pastas[pasta].copypasta);
+				break find;
 			}
 		}
+			if (msg.content.includes("!matrix")){
+				let elements = msg.content.slice(8).split(" ");
+				let mat = new Matrix3(elements);
+				msg.reply(solveMatrix(mat));
+			}
+		}
+		
 	}
 });
 
 client.login(tokenenv);
 
+function stringMat(matr){
+	let a = matr.array
+	return `${a[0][0]}	${a[0][1]}	${a[0][2]}\n${a[1][0]}	${a[1][1]}	${a[1][2]}\n${a[2][0]}	${a[2][1]}	${a[2][2]}\n`
+}
+
+function solveMatrix(mat){
+	let response = "Resolución de la matriz por método de Gauss\n";
+	response+=stringMat(mat);
+	let solution = mat.triangulize();
+	response+="La tercera fila se multiplica por " + solution[1][0]+ ", después se resta de la segunda fila.\n"
+	response+=stringMat(solution[0][0]);
+	response+=`La segunda fila se multiplica por ${solution[1][1]}, después se resta de la primera fila. \n`
+	response+=stringMat(solution[0][1]);
+	response+=`La tercera fila se multiplica por ${solution[1][2]}, después se resta de la segunda fila.\n`
+	response+=stringMat(solution[0][2]);
+	response+="Matriz triangulizada :D"
+	return response;
+}
---
+++ gauss.js
@@ -0,0 +1,39 @@
+//class that is a 3x3 matrix
+class Matrix3{
+
+	constructor(list){
+		this.array = [[list[0], list[1], list[2]],
+					  [list[3], list[4], list[5]],
+					  [list[6], list[7], list[8]]];
+	}
+
+	istriangular(){
+		let ar = this.array;
+		return (ar[1][0]===0 && ar[2][0]===0 && ar[2][1]===0)
+	}
+	makeelementzero(i, j){
+		//this function takes an element and makes operations to turn
+		//it into zero
+		//returns a list [matrix, factor multiplied by]
+		let el = JSON.parse(JSON.stringify(this.array[i][j]));
+		let upel = JSON.parse(JSON.stringify(this.array[i-1][j]));
+		let n_mat = JSON.parse(JSON.stringify(this.array));
+		for (let j=0; j<n_mat[i].length; j++){
+			n_mat[i][j] = n_mat[i-1][j] - n_mat[i][j]*upel/el;
+		}
+		return [new Matrix3(n_mat.flat()), upel/el];
+	}
+
+	triangulize(){
+		let first = this.makeelementzero(2, 0)[0];
+		let first_factor = this.makeelementzero(2, 0)[1];
+		let second = first.makeelementzero(1, 0)[0];
+		let second_factor = first.makeelementzero(1, 0)[1];
+		let third = second.makeelementzero(2, 1)[0];
+		let third_factor = second.makeelementzero(2, 1)[1];
+
+		return [[first, second, third],[first_factor, second_factor, third_factor]]
+	}
+}
+
+module.exports = {Matrix3};
